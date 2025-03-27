539abfe4be0ddef0d8f8069baeb4dd09f1cacffb
jomchot <jomchot.j@gmail.com>
2025-03-27 23:37:03 +0700

    first

---
+++ miniproject/AppShell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using miniproject.pages;
+
+namespace miniproject;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+		Routing.RegisterRoute(nameof(ProfilePage), typeof(ProfilePage));
+		Routing.RegisterRoute(nameof(RegisterPage), typeof(RegisterPage));
+
+	}
+}
